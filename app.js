228d47a033f993e1fe3445187d5ea2fc08e190a2
Jamie <kookjc6@gmail.com>
2020-01-09 13:15:20 -0600

    added switch operator to create object based on user input

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-// import Manager from "./lib/Manager";
-// import Engineer from "./lib/Engineer"; 
-// import Intern from "./lib/Intern"; 
+const Manager = require( "./lib/Manager");
+const Engineer = require( "./lib/Engineer"); 
+const Intern = require( "./lib/Intern"); 
 const Inquirer = require("inquirer"); 
 
 const questionFilter= { "Manager": "office number",
                         "Engineer": "Github username",
-                        "Intern": "school"}; 
-
-const questionKey=  { "Manager": "officeNumber",
-                        "Engineer": "github",
-                        "Intern": "school"};
+                        "Intern": "school's name"}; 
 
 let type= null; 
 
 function askUserInput(){
-    console.log("\n\nWelcome to the Team Summary Developer. \n\n To build your team, please answer the following questions: "); 
+    if (type= null){
+        console.log("\n\nWelcome to the Team Summary Developer. \n\n To build your team, please answer the following questions: "); 
+    }
     Inquirer
         .prompt([
          {
@@ -25,46 +23,58 @@ function askUserInput(){
          }
         ])
         .then(function(response){
-            console.log(response); 
+            // console.log(response); 
             type= response.memberType; 
-            let typeQuestion= questionFilter[type]; 
-            let typeKey= questionKey[type]; 
+            let typeQuestion= questionFilter[type];  
             if (type !== "I have finished adding my team"){
                 return Inquirer.prompt([
                     {
                         type: "input",
                         message: `What is the name of your ${type}?`,
-                        name: `${type}Name`
+                        name: `Name`
                     },
                     {
                         type: "input",
                         message: `What is the id of this ${type}?`,
-                        name: `${type}Id`
+                        name: `Id`
                     },
                     {
                         type: "input",
                         message: `What is the email of this ${type}?`,
-                        name: `${type}Email`
+                        name: `Email`
                     },
                     {
                         type: "input",
                         message: `What is the ${typeQuestion} of this ${type}?`,
-                        name: `${type}${typeKey}`
+                        name: `Special`
                     }
                 ])
             } else {
-                return ("Thank you for entering your team information \n\n Your summary will be generated shortly."); 
-                
+                console.log("\n\nThank you for entering your team information \n\n Your summary will be generated shortly."); 
+                return "done"
             }
         })
         .then(function(data){
-            if (data !== "Thank you for entering your team information \n\n Your summary will be generated shortly."){
-                let employee= type + data[type+"Name"];
+            if (data !== "done"){
+                // let employee= type + data[type+"Name"];
+                // console.log(employee);
+                let employee= null;   
+                switch(type) {
+                    case "Manager": 
+                        employee = new Manager(data.Name, data.Id, data.Email, data.Special); 
+                        break; 
+                    case "Engineer": 
+                        employee = new Engineer(data.Name, data.Id, data.Email, data.Special); 
+                        break; 
+                    case "Intern": 
+                        employee = new Intern(data.Name, data.Id, data.Email, data.Special); 
+                        break; 
+                    default: 
+                        console.log("Something went wrong"); 
+                        break; 
+                }
                 console.log(employee); 
-                // switch(type) {
-                //     case "Manager": 
-                //         const 
-                // }
+                askUserInput();
             }
         })
 }
